26631e0eafca6cf13d5850caeeb35cbed8215532
LuisaFC <luisabsb@gmail.com>
2024-12-14 18:54:11 -0300

    Dark e Light Mode with shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
+import { ThemeProvider } from "@app/contexts/ThemeContext"
 import { Router } from "@app/Router"
 import { routes } from "@app/Router/routes"
+import { ThemeContext } from "@views/components/ThemeSwitcher"
 import { BrowserRouter, Link } from "react-router-dom"
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <Link to='/'>Home</Link>
-      <br />
-      <Link to={routes.createUser}>Create User</Link>
-      <Router />
-    </BrowserRouter>
+   <ThemeProvider  defaultTheme="dark" storageKey="theme">
+      <BrowserRouter>
+        <Link to='/'>Home</Link>
+        <br />
+        <Link to={routes.createUser}>Create User</Link>
+        <ThemeContext />
+        <Router />
+      </BrowserRouter>
+   </ThemeProvider>
   )
 }
 
